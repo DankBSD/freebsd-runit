d286a754a1a7a74fdcac7267971191e565a9d71c
Greg V <greg@unrelenting.technology>
2020-06-11 21:24:54 +0300

    Do not mount / read-write

--- etc/runit/core-services/31-mount.sh
+++ etc/runit/core-services/31-mount.sh
@@ -1,7 +1,7 @@
 [ -n "${JAILED}" ] && return 0
 
-echo "=> Mounting / read-write"
-mount -uw / || emergency_shell
+# echo "=> Mounting / read-write"
+# mount -uw / || emergency_shell
 
 echo "=> Mounting all non-network filesystems"
 mount -a || emergency_shell
